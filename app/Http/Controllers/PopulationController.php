b2a0ac5ffc3aadcb942103be75ac039fc7b81a7d
Rodrigo Soriano <rsoriano.jose@gmail.com>
2022-05-25 11:31:20 -0600

    populationController created

---
+++ app/Http/Controllers/PopulationController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Population;
+use Illuminate\Http\Request;
+
+class PopulationController extends Controller
+    {
+        public function index()
+            {
+                $population = Population::all();
+                return $population;
+            }
+
+        public function store(Request $request)
+            {
+                $population = new Population();
+
+                $population->states                     = $request->states;
+                $population->total_population           = $request->total_population;
+                $population->unvaccinated_population    = $request->unvaccinated_population;
+                $population->vaccinated_population      = $request->vaccinated_population;
+
+                $population->save();
+
+                return $population;
+            }
+
+        public function show($id)
+            {
+                $population = Population::find($id);
+                return $population;
+            }
+
+        public function update(Request $request, $id)
+            {
+                $population = Population::findOrFail($request->id);
+
+                $population->states                     = $request->states;
+                $population->total_population           = $request->total_population;
+                $population->unvaccinated_population    = $request->unvaccinated_population;
+                $population->vaccinated_population      = $request->vaccinated_population;
+
+                $population->save();
+
+                return $population;
+            }
+
+        public function destroy($id)
+            {
+                $population = Population::destroy($id);
+                return $population;
+            }
+    }
+
+?>
