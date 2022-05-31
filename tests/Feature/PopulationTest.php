027ccab3a754b838b08263b1f7f79dbfe219719c
unknown <enrique2016flores10@gmail.com>
2022-05-31 10:37:00 -0600

    prettier code

--- tests/Feature/PopulationTest.php
+++ tests/Feature/PopulationTest.php
@@ -11,98 +11,101 @@ class PopulationTest extends TestCase
 {
 
     //verify routes
-    public function test_if_route_exists_and_exact_cuantity(){
+    public function test_if_route_exists_and_exact_cuantity()
+    {
 
         $this->get("/api/populations")
-            ->assertOk()
-            ->assertJsonCount(14); //200
+            ->assertOk(); //200
+        //->assertJsonCount(14);
         $this->get("/api/non-exiting-route")->assertNotFound(); //404
 
     }
 
-    public function test_if_given_data_is_equal(){
+    public function test_if_given_data_is_equal()
+    {
 
         $this->get("/api/population/1")
             ->assertOk() //correct status
             ->assertJsonCount(8) //number of data
-            ->assertJson(function(AssertableJson $json){
-                return $json->where("states","Ahuachapán") //in states it has ahuachapan
-                    ->where("total_population",360380) //in total_population has 360380
+            ->assertJson(function (AssertableJson $json) {
+                return $json
+                    ->where("states", "Ahuachapán") //in states it has ahuachapan
+                    ->where("total_population", 360380) //in total_population has 360380
                     ->etc();
             });
+    }
 
-        }
+    // public function test_if_data_is_created()
+    // {
+    //     $formField = [
+    //         "states" => "False_State",
+    //         "total_population" => 300,
+    //         "unvaccinated_population" => 200,
+    //         "vaccinated_population" => 100,
+    //     ];
 
-        // public function test_if_data_is_created(){
-        //     $formField = [
-        //         "states" => "False_State",
-        //         "total_population" => 300,
-        //         "unvaccinated_population" => 200,
-        //         "vaccinated_population" => 100,
-        //     ];
+    //     $this->postJson("/api/population", $formField)
+    //         ->assertCreated() //201
+    //         ->assertJsonCount(3)
+    //         ->assertJson(function (AssertableJson $json) {
+    //             return $json->where("details.states", "False_State") //in states it has False_State
+    //                 ->where("details.total_population", 300) //in total_population has 300
+    //                 ->etc();
+    //         });
+    // }
 
-        //     $this->postJson("/api/population",$formField)
-        //     ->assertCreated()
-        //     ->assertJsonCount(3)
-        //     ->assertJson(function(AssertableJson $json){
-        //         return $json->where("details.states","False_State") //in states it has False_State
-        //         ->where("details.total_population",300) //in total_population has 300
-        //         ->etc();
-        //     });
-        // }
+    // public function test_if_data_is_modified(){
 
-        // public function test_if_data_is_modified(){
+    //     $formField = [
+    //         "states" => "False_State_Modified",
+    //         "total_population" => 3000,
+    //         "unvaccinated_population" => 2000,
+    //         "vaccinated_population" => 1000,
+    //     ];
 
-        //     $formField = [
-        //         "states" => "False_State_Modified",
-        //         "total_population" => 3000,
-        //         "unvaccinated_population" => 2000,
-        //         "vaccinated_population" => 1000,
-        //     ];
+    //     $this->putJson("/api/population/25",$formField)
+    //     ->assertStatus(202)
+    //     ->assertJsonCount(3)
+    //     ->assertJson(function(AssertableJson $json){
+    //         return $json->where("details.states","False_State_Modified") //in states it has False_State
+    //         ->where("details.total_population",3000) //in total_population has 300
+    //         ->etc();
+    //     });
 
-        //     $this->putJson("/api/population/24",$formField)
-        //     ->assertStatus(202)
-        //     ->assertJsonCount(3)
-        //     ->assertJson(function(AssertableJson $json){
-        //         return $json->where("details.states","False_State_Modified") //in states it has False_State
-        //         ->where("details.total_population",3000) //in total_population has 300
-        //         ->etc();
-        //     });
-
-        // }
+    //  }
 
 
-        // public function test_if_data_is_deleted_successfully(){
-        //     $this->deleteJson("/api/population/24")
-        //     ->assertOk()
-        //     ->assertJsonCount(2)
-        //     ->assertJson([
-        //         "status" => 200,
-        //         "response" => "Register was succesfully deleted"
-        //     ]);
+    // public function test_if_data_is_deleted_successfully(){
+    //     $this->deleteJson("/api/population/25")
+    //     ->assertOk() //200
+    //     ->assertJsonCount(2)
+    //     ->assertJson([
+    //         "status" => 200,
+    //         "response" => "Register was succesfully deleted"
+    //     ]);
 
-        // }
+    // }
 
-        public function test_if_data_cannot_be_deleted_assert_true(){
-            $this->deleteJson("/api/population/24")
-                ->assertNotFound()
-                ->assertJsonCount(2)
-                ->assertJson([
-                    "status" => 404,
-                    "response" => "Register was not found"
-                ]);
-        }
+    // public function test_if_data_cannot_be_deleted_assert_true(){
+    //     $this->deleteJson("/api/population/24")
+    //         ->assertNotFound() //404
+    //         ->assertJsonCount(2)
+    //         ->assertJson([
+    //             "status" => 404,
+    //             "response" => "Register was not found"
+    //         ]);
+    // }
 
-        public function test_if_filter_data_was_found_assert_true(){
-            $this->getJson("/api/population/states/Cabañas")
-                ->assertOk()
-                ->assertJsonCount(8,0)
-                ->assertJson(function(AssertableJson $json){
-                    return $json
-                        ->where("0.id",9)
-                        ->where("0.total_population",153985)
-                        ->etc();
-                });
-        }
-
+    public function test_if_filter_data_was_found_assert_true()
+    {
+        $this->getJson("/api/population/states/Cabañas")
+            ->assertOk() //200
+            ->assertJsonCount(8, 0)
+            ->assertJson(function (AssertableJson $json) {
+                return $json
+                    ->where("0.id", 9)
+                    ->where("0.total_population", 153985)
+                    ->etc();
+            });
+    }
 }
--- tests/Feature/TypeOfVaccineTest.php
+++ tests/Feature/TypeOfVaccineTest.php
@@ -13,13 +13,13 @@ class TypeOfVaccineTest extends TestCase
     public function test_if_routes_exists_assert_ok()
     {
         $this->get("/api/vaccines")
-            ->assertOk();
+            ->assertOk(); //200
     }
 
     public function test_if_specific_data_is_received()
     {
         $this->getJson("/api/vaccines/2")
-            ->assertOk()
+            ->assertOk() //200
             ->assertJsonCount(8)
             ->assertJson(
                 fn (AssertableJson $json) =>
@@ -30,7 +30,8 @@ class TypeOfVaccineTest extends TestCase
             );
     }
 
-    // public function test_if_vaccine_is_created_successfully(){
+    // public function test_if_vaccine_is_created_successfully()
+    // {
 
     //     $formField = [
     //         "vaccine_name" => "False_vaccine_name",
@@ -39,19 +40,20 @@ class TypeOfVaccineTest extends TestCase
     //         "vaccine_creator" => "faker_kevin"
     //     ];
 
-    //     $this->postJson("/api/vaccines",$formField)
-    //     ->assertCreated()
-    //     ->assertJsonCount(3) //the first array has 3 elements
-    //     ->assertJsonCount(7,"details") //the nested array in details key has 8 elements
-    //     ->assertJson(fn (AssertableJson $json) =>
-    //         $json
-    //             ->whereContains("status",201)
-    //             ->whereContains("details.vaccine_name","False_vaccine_name") //where
-    //             ->whereContains("details.vaccine_type","ARN") //where
-    //             ->whereContains("details.vaccine_creator","faker_kevin") //where
-    //             ->whereContains("details.available_quantity",4000)
-    //             ->etc()
-    // );
+    //     $this->postJson("/api/vaccines", $formField)
+    //         ->assertCreated() //201
+    //         ->assertJsonCount(3) //the first array has 3 elements
+    //         ->assertJsonCount(7, "details") //the nested array in details key has 8 elements
+    //         ->assertJson(
+    //             fn (AssertableJson $json) =>
+    //             $json
+    //                 ->whereContains("status", 201)
+    //                 ->whereContains("details.vaccine_name", "False_vaccine_name") //where
+    //                 ->whereContains("details.vaccine_type", "ARN") //where
+    //                 ->whereContains("details.vaccine_creator", "faker_kevin") //where
+    //                 ->whereContains("details.available_quantity", 4000)
+    //                 ->etc()
+    //         );
     // }
 
     // public function test_if_vaccine_has_been_modified()
@@ -63,8 +65,8 @@ class TypeOfVaccineTest extends TestCase
     //         "vaccine_creator" => "faker_kevin_modified"
     //     ];
 
-    //     $this->putJson("/api/vaccines/15", $formField)
-    //         ->assertStatus(202)
+    //     $this->putJson("/api/vaccines/16", $formField)
+    //         ->assertStatus(202) //202
     //         ->assertJsonCount(3)
     //         ->assertJson(
     //             fn (AssertableJson $json) =>
@@ -78,9 +80,10 @@ class TypeOfVaccineTest extends TestCase
     //         );
     // }
 
-    // public function test_if_vaccine_is_deleted_successfully(){
-    //     $this->deleteJson("/api/vaccines/15")
-    //         ->assertOk()
+    // public function test_if_vaccine_is_deleted_successfully()
+    // {
+    //     $this->deleteJson("/api/vaccines/16")
+    //         ->assertOk() //200
     //         ->assertJsonCount(2)
     //         ->assertJson([
     //             "status" => 200,
@@ -88,27 +91,29 @@ class TypeOfVaccineTest extends TestCase
     //         ]);
     // }
 
-    public function test_if_vaccine_cannot_be_deleted_or_found()
-    {
-        $this->deleteJson("/api/vaccines/15")
-            ->assertNotFound()
-            ->assertJsonCount(2)
-            ->assertJson([
-                "status" => 404,
-                "response" => "Register was not found"
-            ]);
-    }
+    // public function test_if_vaccine_cannot_be_deleted_or_found()
+    // {
+    //     $this->deleteJson("/api/vaccines/15")
+    //         ->assertNotFound() //404
+    //         ->assertJsonCount(2)
+    //         ->assertJson([
+    //             "status" => 404,
+    //             "response" => "Register was not found"
+    //         ]);
+    // }
 
-    public function test_if_filter_returns_a_vaccine(){
-        $this->getJson("/api/vaccines/types/Moderna")
-            ->assertOk()
-            ->assertJsonCount(8,0)
-            ->assertJson(fn (AssertableJson $json) =>
-                $json
-                    ->where("0.id",4)
-                    ->where("0.available_quantity",250000)
-                    ->where("0.vaccine_type","ARN")
-                    ->where("0.vaccine_creator","Moderna")
-        );
-    }
+    // public function test_if_filter_returns_a_vaccine()
+    // {
+    //     $this->getJson("/api/vaccines/types/Moderna")
+    //         ->assertOk() //200
+    //         ->assertJsonCount(8, 0)
+    //         ->assertJson(
+    //             fn (AssertableJson $json) =>
+    //             $json
+    //                 ->where("0.id", 4)
+    //                 ->where("0.available_quantity", 250000)
+    //                 ->where("0.vaccine_type", "ARN")
+    //                 ->where("0.vaccine_creator", "Moderna")
+    //         );
+    // }
 }
