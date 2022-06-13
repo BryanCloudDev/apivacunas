7fde16ee729ba231d75e95f67af175e7804c3695
Bryan Portillo <bportillo701@gmail.com>
2022-06-13 01:31:08 -0600

    adding non-duplicated feature to register method

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,44 +6,46 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 use App\Models\User;
 class RegisterController extends Controller
 {
     public function register(Request $request){
 
-        $post_data = $request->validate([
-                'name'=>'required|string',
-                'email'=>'required|string|email|unique:users',
-                'password'=>'required|min:8'
+        $validator = Validator::make($request->all(), [
+            'name'=>'required|string',
+            'email'=>'required|string|email|unique:users',
+            'password'=>'required|min:8'
         ]);
- 
-            $user = User::create([
-            'name' => $post_data['name'],
-            'email' => $post_data['email'],
-            'password' => Hash::make($post_data['password']),
-            ]);
- 
-            $token = $user->createToken('authToken')->plainTextToken;
- 
-            return response()->json([
+
+        if($validator->fails())
+        {
+            return response()->json($validator->errors());
+        }
+        $user = User::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => Hash::make($request['password']),
+        ]);
+        $token = $user->createToken('authToken')->plainTextToken;
+        return response()->json([
             'access_token' => $token,
             'token_type' => 'Bearer',
-            ]);
+        ]);
         }
- 
+
         public function login(Request $request){
-        if (!\Auth::attempt($request->only('email', 'password'))) {
-               return response()->json([
-                'message' => 'Login information is invalid.'
-              ], 401);
+        if (!Auth::attempt($request->only('email', 'password'))) {
+                return response()->json([
+                    'message' => 'Login information is invalid.'
+                ], 401);
         }
- 
+
         $user = User::where('email', $request['email'])->firstOrFail();
-                $token = $user->createToken('authToken')->plainTextToken;
- 
-            return response()->json([
+        $token = $user->createToken('authToken')->plainTextToken;
+        return response()->json([
             'access_token' => $token,
             'token_type' => 'Bearer',
-            ]);
-        }
+        ]);
+    }
 }
